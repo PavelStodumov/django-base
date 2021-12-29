47bbbf00d8bd3b37a873632d571a5ff943099da3
Pavel <stadumoff@yandex.ru>
2021-12-29 09:09:18 +0300

    диспетчер заработал

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -5,6 +5,6 @@ import mainapp.views as mainapp
 app_name = 'mainapp'
 
 urlpatterns = [
-    path('', mainapp.products, name='products_index'),
+    path('', mainapp.products_index, name='products_index'),
     path('<int:pk>/', mainapp.products, name='products'),
 ]
