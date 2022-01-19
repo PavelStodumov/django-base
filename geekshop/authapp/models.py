f974004c5948c134eca7ad0d051a2f7695efb145
Pavel <stodumoff@yandex.ru>
2022-01-19 13:27:54 +0300

    add profile

--- geekshop/authapp/models.py
+++ geekshop/authapp/models.py
@@ -1,7 +1,10 @@
+from tabnanny import verbose
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.timezone import now
 from datetime import timedelta
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class ShopUser(AbstractUser):
@@ -17,3 +20,25 @@ class ShopUser(AbstractUser):
             return False
         else:
             return True
+
+
+class ShopUserProfile(models.Model):
+    MALE = 'M'
+    FEEMALE = 'W'
+    GENDER_CHOICES = ((MALE, 'М'), (FEEMALE, 'Ж'))
+    user = models.OneToOneField(
+        ShopUser, unique=True, null=False, db_index=True, on_delete=models.CASCADE)
+    tagline = models.CharField(verbose_name='Теги', max_length=128, blank=True)
+    about_me = models.TextField(
+        verbose_name='О себе', max_length=512, blank=True)
+    gender = models.CharField(
+        verbose_name='Пол', max_length=1, choices=GENDER_CHOICES, blank=True)
+
+    @receiver(post_save, sender=ShopUser)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            ShopUserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=ShopUser)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.shopuserprofile.save()
